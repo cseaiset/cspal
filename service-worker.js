daddea23b60d07116abebcdb4ecdc193b8acb69c
Mareena Kunjachan <mareenakunjachan@gmail.com>
2019-08-06 21:18:03 +0530

    modify service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,75 @@
+  var CACHE_NAME = 'cspal-v1';
+  var urlsToCache = [
+    '/',
+    '/assets/css/bootstrap.min.css',
+    '/assets/css/main.css',
+    '/assets/css/style.min.css',
+    'assets/css/material.indigo-pink.min.css',
+    'assets/js/mdlDrawerPull.min.js',
+    'assets/js/jquery.touchSwipe.min.js',
+    'assets/js/bootstrap.min.js',
+    'assets/js/material.min.js',
+    'assets/js/popper.min.js'
+  ];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        return fetch(event.request).then(
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // IMPORTANT: Clone the response. A response is a stream
+            // and because we want the browser to consume the response
+            // as well as the cache consuming the response, we need
+            // to clone it so we have two streams.
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+    );
+});
+
+self.addEventListener('activate', function(event) {
+
+  var cacheWhitelist = ['cspal-v1'];
+
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
