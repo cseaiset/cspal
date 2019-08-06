2f71d489e99f6f68b8d609612b2069b8dc4ee6e5
Mareena Kunjachan <mareenakunjachan@gmail.com>
2019-08-06 20:45:28 +0530

    modify service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,9 +2,5 @@ const FILES_TO_CACHE = [
   'offline.html',
 ];
 
-evt.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log('[ServiceWorker] Pre-caching offline page');
-      return cache.addAll(FILES_TO_CACHE);
-    })
-);
+// CODELAB: Precache static resources here.
+evt.waitUntil(caches.open(CACHE_NAME).then((cache) => {console.log('[ServiceWorker] Pre-caching offline page');return cache.addAll(FILES_TO_CACHE);}));
