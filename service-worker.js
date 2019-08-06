5f0a46682c29ca0b6064826a7ac6a05d3b82b896
Mareena Kunjachan <mareenakunjachan@gmail.com>
2019-08-06 21:31:13 +0530

    modify sw

--- service-worker.js
+++ service-worker.js
@@ -59,7 +59,7 @@ self.addEventListener('fetch', function(event) {
 
 self.addEventListener('activate', function(event) {
 
-  var cacheWhitelist = ['cspal-v1'];
+  var cacheWhitelist = [CACHE_NAME];
 
   event.waitUntil(
     caches.keys().then(function(cacheNames) {
