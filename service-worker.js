8c159a7c0937636b0c2439717be3cf8e1a2fa303
Jagannath Bhat <jagannathbhatjb@gmail.com>
2019-08-07 21:56:31 +0530

    fix cache problem

--- service-worker.js
+++ service-worker.js
@@ -7,10 +7,7 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(`static-${version}`)
       .then(cache => Promise.all(
-        [
-          '/styles.css',
-          '/script.js'
-        ].map(url => {
+        urlsToCache.map(url => {
           return fetch(`${url}?${Math.random()}`).then(response => {
             if (!response.ok) throw Error('Not ok');
             return cache.put(url, response);
