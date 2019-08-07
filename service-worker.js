f639c18836a7a3750acba33fc5bf9b5c21533ae6
Jagannath Bhat <jagannathbhatjb@gmail.com>
2019-08-07 22:17:44 +0530

    update

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,15 @@
   var CACHE_NAME = 'cspal-v1';
   var urlsToCache = [
   '/',
-  '*.css',
-  '*.js'
+  'assets/css/main.css',
+  'assets/css/bootstrap.min.css',
+  'assets/css/material.indigo-pink.min.css',
+  'assets/css/style.min.css',
+  'assets/css/icon.css',
+  'assets/css/css.css',
+  'assets/js/bootstrap.min.js',
+  'assets/js/material.min.js',
+  'assets/js/popper.min.js'
   ];
 
 self.addEventListener('install', event => {
