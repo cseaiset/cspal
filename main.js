33efb3b85fd4495d2b1d51dd1bd18970ad443649
Mareena Kunjachan <mareenakunjachan@gmail.com>
2019-08-13 17:18:52 +0530

    update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ function displayNotification() {
     navigator.serviceWorker.getRegistration().then(function(reg) {
       var options = {
         body: 'Welcome to CSPAL',
-        icon: 'icons/48x48.png',
+        icon: 'assets/icons/48x48.png',
         vibrate: [100, 50, 100],
         data: {
           dateOfArrival: Date.now(),
@@ -17,4 +17,11 @@ function displayNotification() {
       reg.showNotification('New Message', options);
     });
   }
+ else if (Notification.permission === "blocked") {
+ } 
+ else {
+	Notification.requestPermission(function(status) {
+    console.log('Notification permission status:', status);
+	});
+ }
 }
