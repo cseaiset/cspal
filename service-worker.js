3e8d08a994901641871274ece7f6a9c548659189
Mareena Kunjachan <mareenakunjachan@gmail.com>
2019-08-13 14:23:05 +0530

    update sw.js

--- service-worker.js
+++ service-worker.js
@@ -9,20 +9,6 @@ self.addEventListener('install', function(event) {
 
 self.addEventListener('activate', function(event) {
   console.log('Activated sw.js', event);
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.filter(function(cacheName) {
-          return true;
-          // Return true if you want to remove this cache,
-          // but remember that caches are shared across
-          // the whole origin
-        }).map(function(cacheName) {
-          return caches.delete(CACHE_NAME);
-        })
-      );
-    })
-  );
 });
 
 self.addEventListener('fetch', function(event){
