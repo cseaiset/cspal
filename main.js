ffff9d8698f991f7ed9744c1119ad27ed2bfb10b
Mareena Kunjachan <mareenakunjachan@gmail.com>
2019-08-31 17:43:32 +0530

    update index

--- main.js
+++ main.js
@@ -26,16 +26,48 @@ function displayNotification() {
         });
     }
 
-    } else if (Notification.permission == 'blocked') {break;} 
+    else if (Notification.permission == 'blocked') {break;} 
         else {
         Notification.requestPermission(function(status) {
             console.log('Notification permission status:', status);
         });
     }
 }
-if ('Notification' in window && navigator.serviceWorker) {
- displayNotificaion()
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('sw.js').then(function(reg) {
+    console.log('Service Worker Registered!', reg);
+
+    reg.pushManager.getSubscription().then(function(sub) {
+      if (sub === null) {
+        // Update UI to ask user to register for Push
+        console.log('Not subscribed to push service!');
+      } else {
+        // We have a subscription, update the database
+        console.log('Subscription object: ', sub);
+      }
+    });
+  })
+   .catch(function(err) {
+    console.log('Service Worker registration failed: ', err);
+  });
 }
 
+function subscribeUser() {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.ready.then(function(reg) {
 
-displayNotification()
+      reg.pushManager.subscribe({
+        userVisibleOnly: true
+      }).then(function(sub) {
+        console.log('Endpoint URL: ', sub.endpoint);
+      }).catch(function(e) {
+        if (Notification.permission === 'denied') {
+          console.warn('Permission for notifications was denied');
+        } else {
+          console.error('Unable to subscribe to push', e);
+        }
+      });
+    })
+  }
+}
