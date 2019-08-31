fada64c943960aab760f48e2c3ba9199793f7fdb
Mareena Kunjachan <mareenakunjachan@gmail.com>
2019-08-31 12:07:47 +0530

    update main.js

--- main.js
+++ main.js
@@ -3,20 +3,31 @@ Notification.requestPermission(function(status) {
 });
 
 function displayNotification() {
-    if (Notification.permission === "granted") {
-        navigator.serviceWorker.ready.then(function(reg) {
-            var options = {
-                body: 'Welcome to cspal',
-                icon: 'assets/icons/48x48.png',
-                vibrate: [100, 0, 100]
-                data: {
-                    dateOfArrival: Date.now(),
-                    primaryKey: 1
-                }
-            };
-            reg.showNotification('Homeworks Alert', options);
+    if (Notification.permission == 'granted') {
+        navigator.serviceWorker.getRegistration().then(function(reg) {
+        var options = {
+        body: 'Welcome to CSPAL',
+        icon: 'assets/icons/48x48.png',
+        vibrate: [100, 50, 100],
+        data: {
+          dateOfArrival: Date.now(),
+          primaryKey: 1
+        },
+            actions: [
+          {action: 'explore', title: 'Open',
+            icon: 'assets/icons/checkmark.png'},
+          {action: 'close', title: 'Close',
+            icon: 'assets/icons/crossmark.png'},
+        ]
+
+      };
+
+        reg.showNotification('Hello world!', options);
         });
-    } else if (Notification.permission === "blocked") {} else {
+    }
+
+    } else if (Notification.permission == 'blocked') {break;} 
+        else {
         Notification.requestPermission(function(status) {
             console.log('Notification permission status:', status);
         });
