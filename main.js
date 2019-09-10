bee103b2612f7eaff6d7730418cbf70b734c5439
Mareena Kunjachan <mareenakunjachan@gmail.com>
2019-09-10 20:51:35 +0530

    update main.js

--- main.js
+++ main.js
@@ -1,75 +1,20 @@
-Notification.requestPermission(function(status) {
-    console.log('Notification permission status:', status);
+Notification.requestPermission(result =>  {
+  console.log('Notification permission status:', result);
 });
 
-function displayNotification() {
-    if (Notification.permission == 'granted') {
-        navigator.serviceWorker.getRegistration().then(function(reg) {
-        var options = {
-        body: 'Welcome to CSPAL',
-        icon: 'assets/icons/48x48.png',
-        vibrate: [100, 50, 100],
-        data: {
-          dateOfArrival: Date.now(),
-          primaryKey: 1
-        },
-            actions: [
-          {action: 'explore', title: 'Open',
-            icon: 'assets/icons/checkmark.png'},
-          {action: 'close', title: 'Close',
-            icon: 'assets/icons/crossmark.png'},
-        ]
+Notification.requestPermission(result => {
+  if (result === 'granted') {
+    showNotification('So nice to have you here!', 'Hey there!')
+  }
+});
 
-      };
-
-        reg.showNotification('CSPAL', options);
-        });
-    }
-
-    else if (Notification.permission == 'denied') {} 
-        else {
-        Notification.requestPermission(function(status) {
-            console.log('Notification permission status:', status);
-        });
-    }
-}
-
-displayNotification()
-
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('sw.js').then(function(reg) {
-    console.log('Service Worker Registered!', reg);
-
-    reg.pushManager.getSubscription().then(function(sub) {
-      if (sub === null) {
-        // Update UI to ask user to register for Push
-        console.log('Not subscribed to push service!');
-      } else {
-        // We have a subscription, update the database
-        console.log('Subscription object: ', sub);
-      }
-    });
-  })
-   .catch(function(err) {
-    console.log('Service Worker registration failed: ', err);
-  });
-}
-
-function subscribeUser() {
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.ready.then(function(reg) {
-
-      reg.pushManager.subscribe({
-        userVisibleOnly: true
-      }).then(function(sub) {
-        console.log('Endpoint URL: ', sub.endpoint);
-      }).catch(function(e) {
-        if (Notification.permission === 'denied') {
-          console.warn('Permission for notifications was denied');
-        } else {
-          console.error('Unable to subscribe to push', e);
-        }
+function showNotification(title, message) {
+  if ('Notification' in window) {
+    navigator.serviceWorker.ready.then(registration => {
+      registration.showNotification(title, {
+        body: message,
+        tag: 'vibration-sample'
       });
-    })
+    });
   }
 }
